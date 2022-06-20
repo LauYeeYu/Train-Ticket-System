1c9c7c08bf1f07e8347c6f0f0ec62e7359451289
Lau Yee-Yu <liuyiyu2003@126.com>
2022-06-20 10:50:34 +0800

    prettier look in query

--- index.mjs
+++ index.mjs
@@ -414,7 +414,24 @@ router.post('/query-ticket', async ctx => {
     </head>
     <body>
         <main>
-            `
+        <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <img class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap" src="/icon.svg"></img>
+        </a>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
+          <li><a href="/query-plan" class="nav-link px-2 text-white">Travel Plan</a></li>
+          <li><a href="/profile" class="nav-link px-2 text-white">Profile</a></li>
+          <li><a href="/orders" class="nav-link px-2 text-white">Orders</a></li>
+        </ul>
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2" id="login">Login</button>
+        </div>
+      </div>
+    </div>
+  </header>`
 
     for (let i = 0; i < num; ++i) {
         const [ trainID, from, leavingDate, leavingTime, arrow, to, arrivalDate, arrivalTime, price, seats ] = (await getline()).split(' ')
@@ -479,6 +496,24 @@ router.post('/query-transfer', async ctx => {
     </head>
     <body>
         <main>
+        <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <img class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap" src="/icon.svg"></img>
+        </a>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
+          <li><a href="/query-plan" class="nav-link px-2 text-white">Travel Plan</a></li>
+          <li><a href="/profile" class="nav-link px-2 text-white">Profile</a></li>
+          <li><a href="/orders" class="nav-link px-2 text-white">Orders</a></li>
+        </ul>
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2" id="login">Login</button>
+        </div>
+      </div>
+    </div>
+  </header>
             `
 
         for (let i = 0; i < 2; ++i) {
